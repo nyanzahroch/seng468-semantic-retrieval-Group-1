09df4c556d85963b102435ffeadd74f99be6cc77
Noor Yousef <nooryousef003@gmail.com>
2026-04-15 19:57:30 -0700

    feat: added Celery and Redis foundation

---
+++ src/core/celery_app.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+from src.core.config import settings
+
+
+celery_app = Celery(
+    "semantic_retrieval",
+    broker=settings.celery_broker_url,
+    backend=settings.celery_result_backend,
+)
+
+celery_app.conf.task_default_queue = settings.celery_index_queue
